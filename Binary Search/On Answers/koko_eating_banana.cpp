c0ee63923b4131aa0fea11cd5bc9ce014bc628c5
Balaji Asish <balajiashish02@gmail.com>
2025-07-16 13:58:12 +0530

    Update koko_eating_banana.cpp

--- Binary Search/On Answers/koko_eating_banana.cpp
+++ Binary Search/On Answers/koko_eating_banana.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int celing(int a, int b) { 
-        return (a + b - 1) / b; 
-    }
+        return (a + b - 1) / b; // this returns celing of a division.
+    } // celing(2,1)= (2+1-1)/1=2. 
+      // celing(3,2) should give us 2. (3+2-1)/2=4/2=2.
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1, high = *max_element(piles.begin(), piles.end());
         int ans = high;
