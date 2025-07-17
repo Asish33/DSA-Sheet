6b94a2fa8ba6eed060de6e4c0a4f47e7217529e0
Balaji Asish <balajiashish02@gmail.com>
2025-07-17 12:24:10 +0530

    optimized solution

--- DP/2-DP/Unique Paths.cpp
+++ DP/2-DP/Unique Paths.cpp
@@ -12,3 +12,20 @@ public:
         return fun(m-1,n-1);
     }
 };
+
+//memo solution using dp array of dimension 2. as it involves 2 state variables row & col
+class Solution {
+public:
+    int fun(int row,int col,vector<vector<int>>&dp){
+        if(row==0 && col==0)return 1;
+        if(row<0 || col<0)return 0;
+        if(dp[row][col]!=-1)return dp[row][col];
+        int up = fun(row-1,col,dp);
+        int left = fun(row,col-1,dp);
+        return dp[row][col]=up+left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return fun(m-1,n-1,dp);
+    }
+};
