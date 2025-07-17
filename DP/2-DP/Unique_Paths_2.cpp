cfffc1a85ec5aa08e91b3c1d787ded53410e253b
Balaji Asish <balajiashish02@gmail.com>
2025-07-17 12:45:48 +0530

    Unique_Paths_2 Memoized

--- DP/2-DP/Unique_Paths_2.cpp
+++ DP/2-DP/Unique_Paths_2.cpp
@@ -19,3 +19,28 @@ public:
         return fun(obstacleGrid, row - 1, col - 1);
     }
 };
+
+//memo solution with 2d dp array as it has 2 state variables
+class Solution {
+public:
+    int fun(vector<vector<int>>& grid, int row, int col,
+            vector<vector<int>>& dp) {
+        if (row < 0 || col < 0)
+            return 0;
+        if (grid[row][col] == 1)
+            return 0;
+        if (row == 0 && col == 0)
+            return 1;
+        if (dp[row][col] != -1)
+            return dp[row][col];
+        int up = fun(grid, row - 1, col, dp);
+        int left = fun(grid, row, col - 1, dp);
+        return dp[row][col] = up + left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int row = obstacleGrid.size();
+        int col = obstacleGrid[0].size();
+        vector<vector<int>> dp(row, vector<int>(col, -1));
+        return fun(obstacleGrid, row - 1, col - 1, dp);
+    }
+};
