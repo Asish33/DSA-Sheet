d56b9a01cae7adc7f9546e0b58b627fbfebdc72c
Balaji Asish <balajiashish02@gmail.com>
2025-07-17 12:11:18 +0530

    memo solution

--- DP/House_Robber.cpp
+++ DP/House_Robber.cpp
@@ -14,3 +14,18 @@ public:
 };
 
 //optimised memo solution here
+class Solution {
+public:
+    int fun(vector<int>&nums, int i,vector<int>&dp){
+        if(i<0)return 0;
+        if(i==0)return nums[0];
+        if(dp[i]!=-1)return dp[i];
+        int pick = nums[i]+fun(nums,i-2,dp);
+        int notpick = fun(nums,i-1,dp);
+        return dp[i]=max(pick,notpick);
+    }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return fun(nums,nums.size()-1,dp);
+    }
+};
