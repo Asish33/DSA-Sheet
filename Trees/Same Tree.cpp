23c633acc0399e54f6a6783f3b8f3269ae76b98b
Balaji Asish <balajiashish02@gmail.com>
2025-07-21 14:07:01 +0530

    Format

--- Trees/Same Tree.cpp
+++ Trees/Same Tree.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    bool fun(TreeNode*p , TreeNode*q){
-        if(p==NULL&&q==NULL)return true;
-        if(p==NULL || q==NULL)return false;
-        if(p->val!=q->val)return false;
-        return fun(p->left,q->left)&&fun(p->right,q->right);
+    bool fun(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+            return true;
+        if (p == NULL || q == NULL)
+            return false;
+        if (p->val != q->val)
+            return false;
+        return fun(p->left, q->left) && fun(p->right, q->right);
     }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        return fun(p,q);
-    }
+    bool isSameTree(TreeNode* p, TreeNode* q) { return fun(p, q); }
 };
