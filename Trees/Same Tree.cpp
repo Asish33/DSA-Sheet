6ba99a9562c9bf5c203762b4fe81cc062fd7dcf7
Balaji Asish <balajiashish02@gmail.com>
2025-07-21 14:04:34 +0530

    Create Same Tree.cpp

---
+++ Trees/Same Tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool fun(TreeNode*p , TreeNode*q){
+        if(p==NULL&&q==NULL)return true;
+        if(p==NULL && q!=NULL || p!=NULL && q==NULL)return false;
+        if(p->val!=q->val)return false;
+        return fun(p->left,q->left)&&fun(p->right,q->right);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return fun(p,q);
+    }
+};
