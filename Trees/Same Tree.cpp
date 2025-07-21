244f1a7ebb98718621566e5443e14e9a9d430d7b
Balaji Asish <balajiashish02@gmail.com>
2025-07-21 14:06:04 +0530

    Update Same Tree.cpp

--- Trees/Same Tree.cpp
+++ Trees/Same Tree.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool fun(TreeNode*p , TreeNode*q){
         if(p==NULL&&q==NULL)return true;
-        if(p==NULL && q!=NULL || p!=NULL && q==NULL)return false;
+        if(p==NULL || q==NULL)return false;
         if(p->val!=q->val)return false;
         return fun(p->left,q->left)&&fun(p->right,q->right);
     }
