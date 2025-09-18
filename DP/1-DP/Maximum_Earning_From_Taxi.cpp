be096a7d3ff0d9c61671b62dc13ed3517afb35b3
Balaji Asish <balajiashish02@gmail.com>
2025-09-18 21:48:35 +0530

    Update Maximum_Earning_From_Taxi.cpp

--- DP/1-DP/Maximum_Earning_From_Taxi.cpp
+++ DP/1-DP/Maximum_Earning_From_Taxi.cpp
@@ -22,3 +22,35 @@ public:
         return fun(0, rides.size(), rides);
     }
 };
+
+//memoized code
+
+
+class Solution {
+public:
+    long long fun(int i, int n, vector<vector<int>>& nums, vector<long long>& dp) {
+        if (i >= n) return 0;
+        if (dp[i] != -1) return dp[i];
+
+        
+        long long notpick = fun(i + 1, n, nums, dp);
+
+        
+        long long profit = (long long)(nums[i][1] - nums[i][0] + nums[i][2]);
+
+        // Binary search for next ride whose start >= current end. using lowerbound here because manual while loop pointer moving is causing tle.
+        int pointer = lower_bound(nums.begin() + i + 1, nums.end(), 
+                                  vector<int>{nums[i][1], 0, 0}) - nums.begin();
+
+        long long pick = profit + fun(pointer, n, nums, dp);
+
+        return dp[i] = max(pick, notpick);
+    }
+
+    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
+        sort(rides.begin(), rides.end()); 
+        vector<long long> dp(rides.size() + 1, -1);
+        return fun(0, rides.size(), rides, dp);
+    }
+};
+
